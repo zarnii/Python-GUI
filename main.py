646783717a28a473ab670801bb18ecdb8da804b3
pefffpafff <tryakin_gd1@mail.ru>
2022-02-06 01:05:33 +0300

    update main file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import mainwindow
+from PyQt5 import QtCore
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import *
 
@@ -9,38 +10,26 @@ from PyQt5.QtWidgets import *
 class MainWindow(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
 	"""docstring for MainWindow"""
 	def __init__(self):
-		super().__init__()
+		super(MainWindow, self).__init__()
 		self.setupUi(self)
-		self.pushButton.clicked.connect(self.search_in_DERECTORY)
+		self.pushButton.clicked.connect(self.pressButton)
+		self.populate()
 
 
 	def pressButton(self):
-		self.textBrowser.setText(str('wd'))
+		pass
+
+	def populate(self):
+		path = "C:\\Windows"
+		model =	QtWidgets.QFileSystemModel()
+		model.setRootPath((QtCore.QDir.rootPath()))
+		self.treeView.setModel(model)
+		self.treeView.setSortingEnabled(True)
 
 
-	def search_in_DERECTORY(self):
-		self.textBrowser.setText('')
-		DERECTORY = self.lineEdit.text()
-		DERECTORY_COPY = DERECTORY
-		file_number = 0
 
-		try:
-			fod = {}
-			files_on_DERECTORY = os.listdir(DERECTORY)
-			for file in files_on_DERECTORY:
-				info_adout_file = os.path.getsize(f'{DERECTORY}/{file}')
-				file_number += 1
-				'''fod[f'{file}'] = info_adout_file
-				sorted_fod = sorted(fod.items(), key=lambda x: x[1])
-				sorted_fod = dict(sorted_fod)
-				#print(sorted_fod)
-				#sorted_tuple = sorted(fod.items(), key=lambda x: x[1])'''
-				self.textBrowser.append(f'{file_number}) {file}| размер: {info_adout_file} byte')
-		except FileNotFoundError:
-			QMessageBox.information(self, 'Поиск', "Системе не удалось найти данный путь!", QMessageBox.Cancel)
-		except NotADirectoryError:
-			QMessageBox.information(self, 'Открытие файла', "Нельзя открыть файл!", QMessageBox.Cancel)
 
+	
 
 if __name__ == '__main__':
 	#Создание приложения
